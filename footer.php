ff736cc5a54ad02bd15a3edb94a7e8903c7b4b42
mediadei-james <user@Users-MacBook-Pro.local>
2015-10-14 19:29:10 -0400

    initial core files
    Initial commit of all basic core files

---
+++ footer.php
@@ -0,0 +1,14 @@
+		<footer>
+			<div class="mediadei">
+				&copy; <?php echo date("Y"); ?>, ORGANIZATION | <a href="http://www.mediadei.org">Designed and built by Media Dei</a>
+			</div>
+
+		</footer>
+
+
+	</div> <?php #div.page-wrap ?>
+
+
+	<?php wp_footer(); ?>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+
+<head>
+
+	<meta charset='<?php bloginfo('charset') ?>'>
+	<?php if (is_search()) { ?>
+		<meta name="robots" content="noindex, nofollow">
+	<?php }?>
+	<meta name="viewport" content="width=device-width">
+	<link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/global.css" type="text/css">
+	<link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> RSS Feed" href="<?php bloginfo('rss2_url'); ?>">
+
+	<title>Default Theme</title>
+
+
+
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+
+	<div class="page-wrap">
